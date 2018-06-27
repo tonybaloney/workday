73f84e3c57c7681895f2e493adaf5dee95356e01
Anthony Shaw <anthonyshaw@apache.org>
2018-06-27 17:04:30 +1000

    try another path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,10 +17,8 @@ import sys
 cwd = os.getcwd()
 project_root = os.path.dirname(cwd)
 
-# Insert the project root dir as the first element in the PYTHONPATH.
-# This lets us ensure that the source package is imported, and that its
-# version is used.
-sys.path.insert(0, os.path.join(project_root, 'src'))
+sys.path.append(os.path.abspath('../src'))
+
 import workday
 
 # -- Project information -----------------------------------------------------
